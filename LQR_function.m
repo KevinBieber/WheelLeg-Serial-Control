09c4d0cf3aaea10e2a1575ac9e600dcfbc43f60d
jianghaoran <1289691887@qq.com>
2026-01-17 22:10:32 +0800

    改正方程，各参数还需要测

--- LQR_function.m
+++ LQR_function.m
@@ -15,28 +15,33 @@ sin_phi_alpha = sin(phi - alpha);
 cos_phi_alpha = cos(phi - alpha);
 sin_theta = sin(theta);
 cos_theta = cos(theta);
-sin_phi_gama = sin(phi - gama);
-cos_phi_gama = cos(phi - gama);
+% sin_phi_gama = sin(phi - gama);
+% cos_phi_gama = cos(phi - gama);
+sin_phi_gama = sin(phi + gama);
+cos_phi_gama = cos(phi + gama);
 
-const_x_matrix = {Iw, Ip, Im, mw, mp, M, g, R, lm};
-const_matrix = {0.000783725, 0.0010, 0.37623527867, 0.518, 1.258, 15.378, 9.8, 0.055, 0.14458}; % 对应 Iw Ip Im mw mp M g R lm共九个
+const_x_matrix = {Iw, Im, mw, mp, M, g, R, lm};
+const_matrix = {0.000074, 0.00921, 0.200, 0.85, 2.145, 9.8, 0.050, 0.00939}; % 对应 Iw Im mw mp M g R lm共八个
 
 x_matrix = {phi, phi_d, x, x_d, theta, theta_d, T, Tp};
 solution_matrix = {0, 0, 0, 0, 0, 0, 0, 0}; % 刚开始用
 
-MatQ = [80 0 0 0 0 0; 0 20 0 0 0 0; 0 0 20 0 0 0; 0 0 0 30 0 0; 0 0 0 0 3000 0; 0 0 0 0 0 1];
+MatQ = [10 0 0 0 0 0; 0 1 0 0 0 0; 0 0 5 0 0 0; 0 0 0 10 0 0; 0 0 0 0 1000 0; 0 0 0 0 0 1];
 MatR = [20 0; 0 0.25]; %权重矩阵 R 的设计
 
 % 表达式
-Nm = -M * (x_dd - l * (sin_phi_alpha * phi_d2 - cos_phi_alpha * phi_dd) + lm * (- sin_theta * theta_d2 + cos_theta * theta_dd));
-Pm = M * g + M * (l * (-cos_phi_alpha * phi_d2 - sin_phi_alpha * phi_dd) + lm * (-cos_theta * theta_d2 - sin_theta * theta_dd));
+% Nm = -M * (x_dd - l * (sin_phi_alpha * phi_d2 - cos_phi_alpha * phi_dd) + lm * (- sin_theta * theta_d2 + cos_theta * theta_dd));
+Nm = -M * (x_dd - L * (-sin(phi) * phi_d2 + cos(phi) * phi_dd) + lm * (- sin_theta * theta_d2 + cos_theta * theta_dd));
+% Pm = M * g + M * (l * (-cos_phi_alpha * phi_d2 - sin_phi_alpha * phi_dd) + lm * (-cos_theta * theta_d2 - sin_theta * theta_dd));
+Pm = M * g + M * (L * (-cos(phi) * phi_d2 - sin(phi) * phi_dd) + lm * (-cos_theta * theta_d2 - sin_theta * theta_dd));
 
 eq_N = Nm - N == mp * (x_dd - l * (sin_phi_alpha * phi_d2 - cos_phi_alpha * phi_dd));
 eq_P = P - Pm == mp * g + mp * l * (-cos_phi_alpha * phi_d2 - sin_phi_alpha * phi_dd);
 N = solve(eq_N, N);
 P = solve(eq_P, P); % 得到N和P的表达式
 
-eq_phi_dd = Ip * phi_dd == Tp - T + l * cos_phi_alpha * N + l * sin_phi_alpha * P - lp * sin_phi_gama * Pm + lp * cos_phi_gama * Nm;
+% eq_phi_dd = Ip * phi_dd == Tp - T + l * cos_phi_alpha * N + l * sin_phi_alpha * P - lp * sin_phi_gama * Pm + lp * cos_phi_gama * Nm;
+eq_phi_dd = Ip * phi_dd == Tp - T + l * cos_phi_alpha * N + l * sin_phi_alpha * P + lp * sin_phi_gama * Pm + lp * cos_phi_gama * Nm;
 eq_x_dd = x_dd == (N * R + T) / (Iw / R + mw * R);
 eq_theta_dd = Im * theta_dd == -Tp + Pm * lm * sin_theta + Nm * lm * cos_theta;
 
@@ -49,17 +54,12 @@ f3 = phi_x_theta.theta_dd;
 %%考虑不同摆角时
 data_set = readmatrix("data_set.csv");
 data_num = length(data_set(:,1));
-% L_matrix = data_set(:, 1);
-% l_matrix = data_set(:, 2);
-% lp_matrix = data_set(:, 3);
-% alpha_matrix = data_set(:, 4);
-% gama_matrix = data_set(:, 5);
-phi_0_matrix = data_set(:, 6);
+phi_0_matrix = data_set(:, 7);
 
-solution = zeros(20, 8);
+solution = zeros(data_num, 8);
 solution(:,1) = phi_0_matrix;
 
-L_const_x_matrix = {L, l, lp, alpha, gama};
+L_const_x_matrix = {L, l, lp, alpha, gama, Ip};
 
 for i = 1 : 2
     for j = 1 : 6
@@ -70,7 +70,7 @@ for i = 1 : 2
 end
 
 for i = 1 : data_num
-    L_const_matrix = data_set(i, 1:5);
+    L_const_matrix = [data_set(i, 1:6)];
 
     A1 = subs(diff(f1, phi), x_matrix, solution(i, :));
     A2 = subs(diff(f1, theta), x_matrix, solution(i, :));
@@ -119,3 +119,5 @@ for i = 1 : data_num
         end
     end
 end
+
+fprintf("K矩阵已保存\n")
--- output.m
+++ output.m
@@ -5,5 +5,6 @@ p_matrix = readmatrix("curve_p.csv");
 
 for i = 1:12
     p = p_matrix(i,:);
-    fprintf("K_data[%d] = %.4f * L_3 + %.4f * L_2 + %.4f * L_1 + %.4f;\n", i - 1, p(1), p(2), p(3), p(4));
+    fprintf("K_data[%d] = %.4ff * L_3 + %.4ff * L_2 + %.4ff * L_1 + %.4ff;\n", i - 1, p(1), p(2), p(3), p(4));
+    
 end
